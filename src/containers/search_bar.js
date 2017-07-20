e45b575b4dab4309ae2a663cd3b0b1ff25aa0e46
Daniel Velez <velez.a.daniel@gmail.com>
2017-07-20 00:11:39 -0400

    search bar created

---
+++ src/containers/search_bar.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export default class SearchBar extends Component {
+  render() {
+
+    return (
+      <form className="input-group">
+        <input />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondery">Submit</button>
+          </span>
+      </form>
+    )
+  }
+}
